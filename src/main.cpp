03c941c685f2ccc2f61f6c91b28eb9a0e242d445
Incomplete <incomplete@aixon.co>
2020-04-18 16:18:31 +0800

    Finish TODO 1: read start and end from stdin

--- src/main.cpp
+++ src/main.cpp
@@ -51,16 +51,28 @@ int main(int argc, const char **argv)
         else
             osm_data = std::move(*data);
     }
-    
-    // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
-    // user input for these values using std::cin. Pass the user input to the
-    // RoutePlanner object below in place of 10, 10, 90, 90.
+
+    // TODO 1 DONE: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
+    //   user input for these values using std::cin. Pass the user input to the
+    //   RoutePlanner object below in place of 10, 10, 90, 90.
+    float start_x = 0.0;
+    float start_y = 0.0;
+    float end_x = 0.0;
+    float end_y = 0.0;
+    std::cout << "Please input start_x, start_y, end_x and end_y, separate them with whitespace:" << std::endl;
+    if (std::cin >> start_x >> start_y >> end_x >> end_y) {
+        std::cout << "Start: (" << start_x << ", " << start_y << ") End: (" << end_x << ", " << end_y << ")" << std::endl;
+    } else {
+        std::cout << "Failed to parse your input" << std::endl;
+        return 1;
+    }
+
 
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
